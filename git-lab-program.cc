f3ed3219154122eef846268e289f01b40bfc3063
McCaleb0 <cm044321@ohio.edu>
2023-09-06 10:17:05 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Caleb McAlpine
+ *        Date: 9/5/2023
+ * Description: A C++ program added to git-lab-2 that says hi to Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
